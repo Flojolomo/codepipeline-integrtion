8ec9e1e4eb2c0ef58bb4e342103152000d4b1dd5
Florian Siegel <florian.siegel@netlight.com>
2023-07-06 21:04:30 +0200

    Initial commit

---
+++ test/automated-pipeline-for-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutomatedPipelineForCdk from '../lib/automated-pipeline-for-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/automated-pipeline-for-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutomatedPipelineForCdk.AutomatedPipelineForCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
